53df865656545f7b3e2f17466cfb3d6f987ca601
hohlovscky2017 <hohlovskiido@gmail.com>
2022-11-15 12:59:22 +0300

    Сделал форму регистрации

---
+++ Demo_15_11_2022_cvety/Windows/RegWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo_15_11_2022_cvety.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegWindow.xaml
+    /// </summary>
+    public partial class RegWindow : Window
+    {
+        public RegWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonReg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

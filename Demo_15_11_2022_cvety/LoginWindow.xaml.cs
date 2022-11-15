da633fe8076aa0dc5c7dfdb314cbc72742bc69b1
hohlovscky2017 <hohlovskiido@gmail.com>
2022-11-15 11:31:22 +0300

    Добавил код авторизации и исправил перепутанные поля

--- Demo_15_11_2022_cvety/LoginWindow.xaml.cs
+++ Demo_15_11_2022_cvety/LoginWindow.xaml.cs
@@ -26,7 +26,22 @@ namespace Demo_15_11_2022_cvety
 
         private void ButtonLogin_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrEmpty(TextBoxUsername.Text) || string.IsNullOrEmpty(TextBoxPassword.Text)) // если поля textBox1 и textBox2, что соответствует логину и паролю, то выводим сообщение об ошибке
+            {
+                MessageBox.Show("Введите логин и пароль!"); // вывод сообщение об ошибке и прерываем выполнение программы
+                return; // прерываем выполнение программы
+            }
+            using (var db = new cvetyEntities()) // записываем в переменную db подключение к базе данных
+            {
+                var user = db.User.AsNoTracking().FirstOrDefault(u => u.UserLogin == TextBoxUsername.Text && u.UserPassword == TextBoxPassword.Text);
+                MessageBox.Show("Добро пожаловать " + user.Role.RoleName + " " + user.UserSurname + " " + user.UserName + " " + user.UserPatronymic);
 
+                if (user == null) // если переменная users равна null, то есть ничему это означает, что пользователя с такими данными в базе данных не существует, значит выводим сообщение об ошибке
+                {
+                    MessageBox.Show("Польователь с такими данными не найден!"); // вывод сообщение об ошибке и прерываем выполнение программы
+                    return; // прерываем выполнение программы
+                }                
+            }
         }
     }
 }
